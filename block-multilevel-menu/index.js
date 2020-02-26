6c9ab7f5442aa6763ce9cc1147edf981848442ec
Kelina Shrestha <kylesth@gmail.com>
2020-02-26 22:29:08 +0545

    Create multilevel with block menu and mobile responsive.

---
+++ block-multilevel-menu/index.js
@@ -0,0 +1,25 @@
+$(function() {
+  // ------------------------------------------------------- //
+  // Multi Level dropdowns
+  // ------------------------------------------------------ //
+  $("#toggle-menu").on("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    $('#menu-block').toggleClass('d-none');
+
+  });
+
+  $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+    if (!$(this).next().hasClass('show')) {
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+    }
+    var $subMenu = $(this).next(".dropdown-menu");
+    $subMenu.toggleClass('show');
+
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+        $('.dropdown-submenu .show').removeClass("show");
+    });
+
+    return false;
+});
+});
